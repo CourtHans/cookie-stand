8b3a9668c63c16b66f0325d25dd3a89c965d4c2d
Court Hans <cornta@yahoo.com>
2020-06-08 20:29:01 -0700

    got Seattle to work

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 My week 2 project entitled "Cookie Stand" for selling Salmon cookies. Ew.
 
 ## Author: Courtney Hans
+Special thanks to fellow student, Richard Lee for all his help on 6.8.20
 
 
 ## Links and Resources
--- js/app.js
+++ js/app.js
@@ -6,22 +6,57 @@
 //=========global variables============
 var operatingHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 //random number generator inclusive of minimum number https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-function generateRandomNumber(min,max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min 
+function generateRandomNumber(min, max) {
+  return Math.floor(Math.random() * ((max + 1) - min) + min);
 }
 
-var seattleStats = {
-  location : 'Seattle',
-  minNumSeattle: 23,
-  maxNumSeattle: 64,
-  avgCookiePerCust : 6.3,
-  dailyHourSales : [], //will hold the sales numbers calculated via calculateEachHourCookieSales loop below in an array
-  dailyTotalSales: 
-  calculateEachHourCookieSales : function {
-    for(var = i; operatingHours.length; i++){
-      var footTraffic = generateRandomNumber(this.minNumSeattle, this.maxNumSeattle);
-      var hourlySalesTotal = Math.round(footTraffic * this.avgCookiePerCust);
-      this.dailyHourSales.push(hourlySalesTotal);
-    }
+function hourlyTraffic(seattleStats) {
+  for (var i = 0; i < operatingHours.length; i++) {
+    var footTraffic = generateRandomNumber(seattleStats.minNumSeattle, seattleStats.maxNumSeattle);
+    var hourlySalesTotal = Math.round(footTraffic * seattleStats.avgCookiePerCust);
+    seattleStats.dailyHourSales.push(hourlySalesTotal);
+    seattleStats.dailyTotalSales += hourlySalesTotal;
   }
 }
+
+//===========Seattle store==============
+var seattleStats = {
+  location: 'Seattle',
+  minNumSeattle: 23,
+  maxNumSeattle: 64,
+  avgCookiePerCust: 6.3,
+  dailyHourSales: [], //will hold the sales numbers calculated via calculateEachHourCookieSales loop below in an array
+  dailyTotalSales: 0, //will hold the DAILY sales numbers calculated via calculateDailySales loop below
+  // calculate daily total sales by adding all hourly sales
+
+
+  renderToPage: function () {
+    //Step 1: identify target
+    var storeList = document.getElementById('seattle-list');
+    //Step 2: create new element
+    var storeStats = document.createElement('h2');
+    //Step 2.5: get content
+    storeStats.textContent = this.location;
+    //Step 3: insert element/content into target
+    storeList.appendChild(storeStats);
+
+    //write list of hours and hourly sales
+    for (var i = 0; i < operatingHours.length; i++) {
+      storeStats = document.createElement('li');
+      storeStats.textContent = operatingHours[i] + ' : ' + this.dailyHourSales[i] + ' cookies';
+      console.log(this.calculateEachHourCookieSales);
+      storeList.appendChild(storeStats);
+    }
+    storeStats = document.createElement('li');
+    storeStats.textContent = 'Total : ' + this.dailyTotalSales + ' cookies';
+    storeList.appendChild(storeStats);
+    //add in total sales
+    // storeStats = document.createElement('li');
+    // storeStats = textContent = this.location;
+    // storeList.appendChild()
+  }
+};
+
+console.log('dailyHourSales', seattleStats.dailyHourSales);
+hourlyTraffic(seattleStats);
+seattleStats.renderToPage();
